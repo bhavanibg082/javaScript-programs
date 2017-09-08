7d1b3bc4669a414594269d8be7f5074c0c62cbb9
bhavanibg082 <31777011+bhavanibg082@users.noreply.github.com>
2017-09-08 11:23:32 -0700

    Add files via upload

---
+++ jsf1.js
@@ -0,0 +1,29 @@
+
+//assine all possible literals to different variables
+
+
+    var booleanType =true;
+  console.log("boolean" + typeof(booleanType));
+
+  var integer =5;
+    console.log("integer:"+integer);
+
+  var doubleNum =3.59;
+    console.log("double:"+typeof(doubleNum));
+
+    var char='a';
+    console.log("character:"+typeof(char));
+
+    var string="hello how are you";
+    console.log("string:"+typeof(string));
+
+    var undefinedType;
+     console.log("Undefined: " + typeof(undefinedType));
+
+          var nullableType = null;
+    console.log("Null: " +typeof( nullableType));
+
+     var integerObject = new Number(5);
+     console.log("Integer object: " + typeof(integerObject));
+
+
